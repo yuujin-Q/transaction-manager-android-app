cf4a1131feaec8238bdfae02bbfbbd1309a14f13
yuujin-Q <13521074@mahasiswa.itb.ac.id>
2024-04-01 20:52:15 +0700

    feat: logout from settings page

--- app/src/main/java/com/mog/bondoman/ui/pengaturan/PengaturanFragment.kt
+++ app/src/main/java/com/mog/bondoman/ui/pengaturan/PengaturanFragment.kt
@@ -1,13 +1,16 @@
 package com.mog.bondoman.ui.pengaturan
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import com.mog.bondoman.R
+import com.mog.bondoman.data.connection.SessionManager
 import com.mog.bondoman.databinding.FragmentPengaturanBinding
+import com.mog.bondoman.ui.login.LoginFragment
 
 class PengaturanFragment : Fragment() {
     private var _binding: FragmentPengaturanBinding? = null
@@ -16,22 +19,39 @@ class PengaturanFragment : Fragment() {
     // onDestroyView.
     private val binding get() = _binding!!
 
+    private lateinit var sessionManager: SessionManager
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val pengaturanViewModel =
-            ViewModelProvider(this)[PengaturanViewModel::class.java]
-
         _binding = FragmentPengaturanBinding.inflate(inflater, container, false)
-        val root: View = binding.root
-        
-        return root
+
+        sessionManager = SessionManager.getInstance(
+            requireActivity().applicationContext.getSharedPreferences(
+                SessionManager.PREF_KEY,
+                Context.MODE_PRIVATE
+            )
+        )
+
+        binding.logout.setOnClickListener {
+            onNegativeClick()
+        }
+
+        return binding.root
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun onNegativeClick() {
+        sessionManager.removeAuthToken()
+
+        findNavController().popBackStack(0, false)
+        requireActivity().supportFragmentManager.beginTransaction()
+            .replace(R.id.main_fragment_container, LoginFragment()).commit()
     }
 }
